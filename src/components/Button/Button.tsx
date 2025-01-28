5f227735c1bae4c6b66030d354f9a46686fca886
TolkinSL <tolkinsl@yandex.ru>
2025-01-28 17:22:54 +0300

    DEV005 - добавлен компонент Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,34 @@
+import styles from './Button.module.css';
+import cn from 'classnames';
+
+interface ButtonProps {
+  type: 'primary' | 'secondary' | 'social' | 'price' | 'priceSelected';
+  size:
+    | 'size_207'
+    | 'size_283'
+    | 'size_313'
+    | 'size_454'
+    | 'size_472'
+    | 'size_488';
+  children: React.ReactNode;
+}
+export default function Button(props: ButtonProps) {
+  // const { type, size, children } = props;
+  const { type, size, children } = props;
+
+  const buttonBorder = cn(styles.button, {
+    [styles.buttonPrimary]: type === 'primary',
+    [styles.buttonSecondary]: type === 'secondary',
+    [styles.buttonSocial]: type === 'social',
+    [styles.buttonPrice]: type === 'price',
+    [styles.buttonPriceSelected]: type === 'priceSelected',
+    [styles.size_207]: size === 'size_207',
+    [styles.size_283]: size === 'size_283',
+    [styles.size_313]: size === 'size_313',
+    [styles.size_454]: size === 'size_454',
+    [styles.size_472]: size === 'size_472',
+    [styles.size_488]: size === 'size_488',
+  });
+
+  return <button className={buttonBorder}>{children}</button>;
+}
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,22 @@
 import styles from './Main.module.css';
 import CaptionSvg from '../../assets/mainCaption.svg?react';
+import Button from '../Button/Button';
 export default function Main() {
   return (
     <div className={styles.main}>
       <h1 className={styles.caption}>Own your audience.</h1>
       <CaptionSvg />
+      <section>
+        <p>Turn your audience into email and text message subscribers.</p>
+        <div className={styles.twoButton}>
+          <Button type="primary" size="size_313">
+            Get Started Now
+          </Button>
+          <Button type="secondary" size="size_313">
+            View A Demo
+          </Button>
+        </div>
+      </section>
     </div>
   );
 }
