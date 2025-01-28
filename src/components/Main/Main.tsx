d65cd0c865766652a99402de4bbd600b966bfc1e
TolkinSL <tolkinsl@yandex.ru>
2025-01-28 18:05:19 +0300

    DEV006 - добавлена секция Main - Turn

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,16 @@
 import styles from './Main.module.css';
 import CaptionSvg from '../../assets/mainCaption.svg?react';
+import DotSmallSvg from '../../assets/dotGreenSmall.svg?react';
 import Button from '../Button/Button';
 export default function Main() {
   return (
     <div className={styles.main}>
       <h1 className={styles.caption}>Own your audience.</h1>
       <CaptionSvg />
-      <section>
-        <p>Turn your audience into email and text message subscribers.</p>
+      <section className={styles.sectionTurn}>
+        <p className={styles.sectionTurnTitle}>
+          Turn your audience into email and text message subscribers.
+        </p>
         <div className={styles.twoButton}>
           <Button type="primary" size="size_313">
             Get Started Now
@@ -16,6 +19,16 @@ export default function Main() {
             View A Demo
           </Button>
         </div>
+        <div className={styles.sectionTurnStat}>
+          <DotSmallSvg />
+          <p className={styles.sectionTurnNumber}>1000+</p>
+          <p className={styles.sectionTurnDescription}>
+            creators have already started
+          </p>
+        </div>
+      </section>
+      <section className={styles.sectionWhy}>
+        <h2>Why Creators Love Marico</h2>
       </section>
     </div>
   );
